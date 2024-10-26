79678b7e33d1a759f901fcf265adb72b0b2d13a1
C1333760 Abel Cabral De Arruda <c1333760@interno.bb.com.br>
2024-10-26 20:50:22 -0300

    build: fechou versao

--- src/app/middleware/auth.middleware.ts
+++ src/app/middleware/auth.middleware.ts
@@ -3,12 +3,23 @@ import { Request, Response, NextFunction } from "express";
 const checkCredentials = (req: Request, res: Response, next: NextFunction) => {
     const credencial = req.headers['credencial'];
 
-    if (!credencial || credencial !== process.env.CHAVE_DE_ACESSO) {
+    if (!credencial || credencial !== readFileCredencial()) {
         return res.status(401).json({
             message: 'Propriedade "credencial" ausente no header ou é inválida.'
         });
     }
     next();
+};
+
+const readFileCredencial = () => {
+    const filePath = './credencial.txt';
+    try {
+        let credencial = require('fs').readFileSync(filePath, 'utf8');
+        return credencial;
+    } catch (err: any) {
+        console.log(err?.message)
+        return
+    }
 };
 
 export { checkCredentials };
