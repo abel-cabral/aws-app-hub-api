1910c51da0607323f6e5ea33d91eeb1c04ead943
C1333760 Abel Cabral De Arruda <c1333760@interno.bb.com.br>
2024-10-27 22:09:16 -0300

    (fix): ajuste ao ler .txt de credenciais

--- src/app/middleware/auth.middleware.ts
+++ src/app/middleware/auth.middleware.ts
@@ -1,24 +1,38 @@
 import { Request, Response, NextFunction } from "express";
+import * as path from 'path';
+import * as fs from 'fs';
 
-const checkCredentials = (req: Request, res: Response, next: NextFunction) => {
-    const credencial = req.headers['credencial'];
+const configPath = 'credencial.txt';
 
-    if (!credencial || credencial !== readFileCredencial()) {
+const checkCredentials = async (req: Request, res: Response, next: NextFunction) => {
+    const credencial = req.headers['credencial'] as string; // Assegura que credencial é uma string
+    const localCredencial = await readFileCredencial();
+    
+    if (!credencial) {
         return res.status(401).json({
-            message: 'Propriedade "credencial" ausente no header ou é inválida.'
+            message: 'Propriedade "credencial" ausente no header.'
         });
     }
+
+    console.log(localCredencial)
+    
+    if (credencial !== localCredencial) {
+        return res.status(401).json({
+            message: 'Credencial inválida.'
+        });
+    }
+    
     next();
 };
 
-const readFileCredencial = () => {
-    const filePath = './credencial.txt';
+const readFileCredencial = async () => {
+    const filePath: string = path.resolve(process.cwd(), configPath);
     try {
-        let credencial = require('fs').readFileSync(filePath, 'utf8');
-        return credencial;
+        const data = await fs.promises.readFile(filePath, 'utf8');
+        return data.trim(); // Remove espaços em branco ao redor
     } catch (err: any) {
-        console.log(err?.message)
-        return
+        console.error('Erro ao ler o arquivo de credenciais:', err.message);
+        return null; // Retorna null em caso de erro
     }
 };
 
